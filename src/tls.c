d7921431aa9b0c202fe65651b00b155416684da9
Nickolas Lapp <nick@wolfssl.com>
2016-02-19 09:23:05 -0700

    Fixed conditional compile

--- src/tls.c
+++ src/tls.c
@@ -3217,11 +3217,11 @@ static void TLSX_SessionTicket_FreeAll(SessionTicket* tick)
 #define STK_GET_SIZE         TLSX_SessionTicket_GetSize
 #define STK_WRITE            TLSX_SessionTicket_Write
 #define STK_PARSE            TLSX_SessionTicket_Parse
-#define STK_FREE_ALL         TLSX_SessionTicket_FreeAll
+#define STK_FREE_ALL(stk)    TLSX_SessionTicket_FreeAll((SessionTicket*)stk)
 
 #else
 
-#define STK_FREE_ALL
+#define STK_FREE_ALL(a)
 #define STK_VALIDATE_REQUEST(a)
 #define STK_GET_SIZE(a, b)      0
 #define STK_WRITE(a, b, c)      0
@@ -3872,7 +3872,7 @@ void TLSX_FreeAll(TLSX* list)
 
             case TLSX_SESSION_TICKET:
                 /* Nothing to do. */
-                STK_FREE_ALL((SessionTicket*)extension->data);
+                STK_FREE_ALL(extension->data);
                 break;
 
             case TLSX_QUANTUM_SAFE_HYBRID:
